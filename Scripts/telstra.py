b3824e50d9c8e028a5affaa438be8cb56e41398f
minjay <mjfan@ucar.edu>
2016-01-21 15:47:55 -0800

    update

--- Scripts/telstra.py
+++ Scripts/telstra.py
@@ -1,9 +1,8 @@
 import os
 import numpy as np
-import xgboost as xgb
 import pandas as pd
+import xgboost as xgb
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import KFold
 
 # load data
@@ -25,23 +24,48 @@ df_all = pd.concat([df_train, df_test], axis=0, ignore_index=True)
 
 ## feature engineering
 # df_all
-# we assume that location is numerical
-# it makes sense since given a specific class, the locations show the pattern
-# of a number of segments with consecutive numbers
 myfun = lambda x: int(x.strip('location '))
 df_all['location'] = df_all['location'].apply(myfun)
 
-# df_eve
-df_eve = pd.pivot_table(df_eve, index='id', columns='event_type', aggfunc=len,
+# df_loc
+# contain information
+df_loc_table = pd.pivot_table(df_all, index='id', columns='location', aggfunc=len,
 	fill_value=0)
 
-# df_log
+# df_eve
+df_eve_table = pd.pivot_table(df_eve, index='id', columns='event_type', aggfunc=len,
+	fill_value=0)
+grouped = df_eve[['id', 'event_type']].groupby('id')
+# df_eve_num is not useful!
+myfun = lambda x: len(np.unique(x))
+df_eve_num = grouped.aggregate(myfun)
+myfun = lambda x: max(x.apply(lambda x: x.strip('event_type ')).astype(int)) 
+df_eve_max = grouped.aggregate(myfun)
+df_eve_max.rename(columns={'event_type': 'eve_max'}, inplace=True)
+myfun = lambda x: min(x.apply(lambda x: x.strip('event_type ')).astype(int)) 
+df_eve_min = grouped.aggregate(myfun)
+df_eve_min.rename(columns={'event_type': 'eve_min'}, inplace=True)
+myfun = lambda x: np.std(x.apply(lambda x: x.strip('event_type ')).astype(int)) 
+df_eve_std = grouped.aggregate(myfun)
+df_eve_std.rename(columns={'event_type': 'eve_std'}, inplace=True)
+
+## df_log
 df_log_table = pd.pivot_table(df_log, values='volume', index='id', columns='log_feature', aggfunc=np.sum,
 	fill_value=0)
 df_log_vol_sum = pd.DataFrame(data={'log_vol_sum': df_log_table.sum(axis=1)})
 grouped = df_log[['id', 'log_feature']].groupby('id')
 myfun = lambda x: len(np.unique(x))
 df_log_feat_num = grouped.aggregate(myfun)
+df_log_feat_num.rename(columns={'log_feature': 'log_feat_num'}, inplace=True)
+myfun = lambda x: max(x.apply(lambda x: x.strip('feature ')).astype(int))
+df_log_feat_max = grouped.aggregate(myfun)
+df_log_feat_max.rename(columns={'log_feature': 'log_feat_max'}, inplace=True)
+myfun = lambda x: min(x.apply(lambda x: x.strip('feature ')).astype(int))
+df_log_feat_min = grouped.aggregate(myfun)
+df_log_feat_min.rename(columns={'log_feature': 'log_feat_min'}, inplace=True)
+myfun = lambda x: np.std(x.apply(lambda x: x.strip('feature ')).astype(int))
+df_log_feat_std = grouped.aggregate(myfun)
+df_log_feat_std.rename(columns={'log_feature': 'log_feat_std'}, inplace=True)
 
 # df_res
 df_res_table = pd.pivot_table(df_res, index='id', columns='resource_type', aggfunc=len,
@@ -49,24 +73,34 @@ df_res_table = pd.pivot_table(df_res, index='id', columns='resource_type', aggfu
 grouped = df_res[['id', 'resource_type']].groupby('id')
 myfun = lambda x: len(np.unique(x))
 df_res_num = grouped.aggregate(myfun)
+df_res_num.rename(columns={'resource_type': 'res_num'}, inplace=True)
 
-df_sev = pd.pivot_table(df_sev, index='id', columns='severity_type', aggfunc=len,
+# df_sev
+# It is categorical. It does not have an ordering.
+df_sev_table = pd.pivot_table(df_sev, index='id', columns='severity_type', aggfunc=len,
 	fill_value=0)
 
 # combine
-df_all = df_all.join(df_eve, on='id')
-df_all = df_all.join(df_log_table, on='id')
-df_all = df_all.join(df_log_vol_sum, on='id')
-df_all = df_all.join(df_log_feat_num, on='id')
-df_all = df_all.join(df_res_table, on='id')
-df_all = df_all.join(df_res_num, on='id')
-df_all = df_all.join(df_sev, on='id')
+df_all_cb = df_all.join(df_loc_table, on='id')
+df_all_cb = df_all_cb.join(df_eve_table, on='id')
+df_all_cb = df_all_cb.join(df_eve_max, on='id')
+df_all_cb = df_all_cb.join(df_eve_min, on='id')
+df_all_cb = df_all_cb.join(df_eve_std, on='id')
+df_all_cb = df_all_cb.join(df_log_table, on='id')
+df_all_cb = df_all_cb.join(df_log_vol_sum, on='id')
+df_all_cb = df_all_cb.join(df_log_feat_num, on='id')
+df_all_cb = df_all_cb.join(df_log_feat_max, on='id')
+df_all_cb = df_all_cb.join(df_log_feat_min, on='id')
+df_all_cb = df_all_cb.join(df_log_feat_std, on='id')
+df_all_cb = df_all_cb.join(df_res_table, on='id')
+df_all_cb = df_all_cb.join(df_res_num, on='id')
+df_all_cb = df_all_cb.join(df_sev_table, on='id')
 
 # check NaN
-df_all.isnull().any().any()
+df_all_cb.isnull().any().any()
 
 # drop id
-df_all_no_id = df_all.drop('id', axis=1, inplace=False)
+df_all_no_id = df_all_cb.drop('id', axis=1, inplace=False)
 
 X_all = df_all_no_id.values
 X = X_all[:n_train, :]
@@ -80,19 +114,23 @@ param['num_class'] = num_class
 param['nthread'] = 1
 param['silent'] = 1
 
-param['eta'] = 0.1
+param['eta'] = 0.02
 param['colsample_bytree'] = 0.3
+param['subsample'] = 0.9
 
-num_round = 1000
+num_round = 10000
 
 X_test = X_all[n_train:, :]
 xg_test = xgb.DMatrix(X_test)
 
+# set random seed
+np.random.seed(0)
+
 # k-fold
-k = 10
+k = 5
 y_pred_sum = np.zeros((X_test.shape[0], num_class))
 best_score = []
-kf = KFold(X.shape[0], n_folds=k, shuffle=True, random_state=0)
+kf = KFold(X.shape[0], n_folds=k, shuffle=True)
 i = 0
 for train, val in kf:
   i += 1
@@ -102,7 +140,7 @@ for train, val in kf:
   xg_val = xgb.DMatrix(X_val, y_val)
   evallist  = [(xg_train,'train'), (xg_val,'eval')]
   # train
-  bst = xgb.train(param, xg_train, num_round, evallist, early_stopping_rounds=30)
+  bst = xgb.train(param, xg_train, num_round, evallist, early_stopping_rounds=100)
   best_score += [bst.best_score]
   # predict
   xg_test = xgb.DMatrix(X_test)
