160cbf97255a6d88b306b44a23ee32048572ab39
minjay <mjfan@ucar.edu>
2016-02-28 12:57:10 -0800

    single xgb

--- Scripts/telstra4.py
+++ Scripts/telstra4.py
@@ -48,15 +48,7 @@ for colsample_bytree in set_colsample_bytree:
     			nthread=20, silent=1, eta=0.01, colsample_bytree=colsample_bytree, subsample=subsample, 
     			max_depth=max_depth, max_delta_step=1, gamma=0.1, alpha=0, param_lambda=1, n_fold=35, seed=seed)
 
-			clf1 = LogisticRegression(solver='lbfgs', max_iter=1000, multi_class='multinomial', verbose=1, n_jobs=20)
-			my_clf = gen_clf.my_clf(num_class=num_class, n_fold=35, seed=seed)
-			meta_feat1 = my_clf.predict(clf1, X_categ, y, X_categ_test, 'base') 
-			meta_feat1_1 = np.reshape(np.apply_along_axis(np.argmax, 1, meta_feat1), (-1, 1))
-
-			X_meta = np.concatenate([X_numeric, meta_feat1_1[:n_train, :]], axis=1)
-			X_meta_test = np.concatenate([X_numeric_test, meta_feat1_1[n_train:, :]], axis=1)
-
-			y_pred = my_xgb.predict(X_meta, y, X_meta_test, 'meta')
+			y_pred = my_xgb.predict(X, y, X_test, 'meta')
 			y_pred_sum = y_pred_sum+y_pred
 
 y_pred = y_pred_sum/seed
@@ -64,4 +56,4 @@ y_pred = y_pred_sum/seed
 sub = pd.DataFrame(data={'id':ids, 'predict_0':y_pred[:, 0], 'predict_1':y_pred[:, 1],
 	'predict_2':y_pred[:, 2]}, columns=['id', 'predict_0', 'predict_1', 'predict_2'])
 my_dir = os.getcwd()+'/Telstra/Subs/'
-sub.to_csv(my_dir+'sub1.csv', index=False)
+sub.to_csv(my_dir+'sub4.csv', index=False)
